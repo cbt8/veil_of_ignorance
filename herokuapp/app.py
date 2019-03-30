d301446a1b181ee2db6a4c8a2788bc5f27af6926
Matt Hawley <r.matt.hawley@gmail.com>
2019-03-30 16:38:12 -0500

    heroku init, copy heroku-dependent files into production folder

---
+++ herokuapp/app.py
@@ -0,0 +1,55 @@
+#################################################
+# Dependencies
+#################################################
+import pandas as pd 
+from flask import Flask, jsonify, render_template, redirect
+import pymongo
+import json
+
+#################################################
+# Helper Functions
+#################################################
+
+
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+# Set up Mongo/PyMongo
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# Map Database
+    # db = client.usedCarsDB
+
+# Map collection (Table)
+    # cars_collection = db.cars
+    # trueCar_collection = db.trueCar
+
+
+#################################################
+# Flask Routes
+#################################################
+
+# Home Page
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return render_template("index.html")
+
+
+# About
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
