86931da2a28522493c16453ebb4cab6c651a4d90
yuralice <44645776+yuralice@users.noreply.github.com>
2019-04-08 23:10:18 -0500

    fixed merged changes click appears to work

--- static/js/pie.js
+++ static/js/pie.js
@@ -1,5 +1,8 @@
 var url = "api/veilofignorance";
-<<<<<<< HEAD
+working_population(42);
+ethnicity(42);
+education(42);
+
 ///////////////////first function
 function working_population (stateid) {
   d3.json(url, function(error, response)
@@ -7,19 +10,15 @@ function working_population (stateid) {
     if (error) throw error;
   console.log(response);
   var state_data = response.filter(row => row.StateID== stateid)
-  //console.log(state_data)
   var person_under_18 = parseFloat(state_data[0]["Persons under 18 years, percent"])
-  console.log(person_under_18)
   var person_over_65 = parseFloat(state_data[0]["Persons 65 years and over, percent"])
-  console.log(person_over_65)
   var person_working = 100-(parseFloat(person_under_18, 10)+parseFloat(person_over_65, 10))
-  console.log(person_working)
   var state_name = state_data[0]["State"]
   //pie chart
   var trace1 = {
     labels: ["under 18 years", "working age population", "65 years and over"],
-    orientation: 'h',
-    showlegend: true,
+    //orientation: 'h',
+    //showlegend: true,
     legend: {
       x: 1,
       y: 1
@@ -48,8 +47,6 @@ function working_population (stateid) {
   }
     
    )}; 
-  
-  working_population(42);
 
 ////////////////second function
 function ethnicity (stateid) {
@@ -124,11 +121,7 @@ function ethnicity (stateid) {
 
      };
   
-  
-  //Plotly.newPlot("pie1", data, layout);
   Plotly.newPlot("bar1", data2, layout2);
-  //Plotly.newPlot("pie3", data, layout);
-  
     }
      )}; 
 
@@ -188,9 +181,6 @@ function education (stateid) {
     yxis: {automargin:true}
    };
 
-
-//Plotly.newPlot("pie1", data, layout);
-//Plotly.newPlot("bar1", data2, layout2);
 Plotly.newPlot("bar2", data3, layout3);
 
   }
@@ -199,62 +189,4 @@ Plotly.newPlot("bar2", data3, layout3);
 
 
 
-    working_population(42);
-    ethnicity(42);
-    education(42);
-=======
-
-function working_population (stateid) {
- d3.json(url, function(error, response)
-  {
-   if (error) throw error;
- console.log(response);
- var state_data = response.filter(row => row.StateID== stateid)
- //console.log(state_data)
- var person_under_18 = parseFloat(state_data[0]["Persons under 18 years, percent"])
- console.log(person_under_18)
- var person_over_65 = parseFloat(state_data[0]["Persons 65 years and over, percent"])
- console.log(person_over_65)
- var person_working = 100-(parseFloat(person_under_18, 10)+parseFloat(person_over_65, 10))
- console.log(person_working)
- //pie chart
- var trace1 = {
-   labels: ["under 18 years", "working age population", "65 years and over"],
-   orientation: 'h',
-   showlegend: true,
-   legend: {
-     x: 1,
-     y: 1
-   },
-   values: [person_under_18, person_working, person_over_65],
-   type: "pie",
-   marker:{
-   colors: [
-     'orange',
-     'purple',
-     'red',
-     'blue',
-     'green'
-     ]}
-  };
-
-  var data = [trace1];
-  var layout = {
-   title: "% of people in working age population",
-   height: 500,
-   width: 500
-  };
-
-
-Plotly.newPlot("pie1", data, layout);
-Plotly.newPlot("pie2", data, layout);
-Plotly.newPlot("pie3", data, layout);
-
- }
-
-  )};
-
-
-
- working_population(42);
->>>>>>> a80ee3f9c8936728a85d8dcdaae77df9dfdd46b6
+  
