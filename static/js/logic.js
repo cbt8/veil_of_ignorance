0aa44be800799817ab9f49257a5a0f31c770bd8f
Matt Hawley <r.matt.hawley@gmail.com>
2019-04-09 21:18:30 -0500

    Bullet Charts update now, histogram and bullet charts on same row

--- static/js/logic.js
+++ static/js/logic.js
@@ -32,6 +32,7 @@ var stateObject = topojson.feature(us, us.objects.states).features
         working_population(d.id);
         ethnicity(d.id);
         education(d.id);
+        d3.select("#BulleT_horizontal").html('');
         bulletdata(d.id);
       })
 
